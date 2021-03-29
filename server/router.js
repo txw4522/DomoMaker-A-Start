5062674dc87c8698b69b8f079f87cbe004033977
Tony Weng <txw4522@g.rit.edu>
2021-03-28 22:59:52 -0400

    DONE DOMOMAKER A

---
+++ server/router.js
@@ -0,0 +1,14 @@
+const controllers = require('./controllers');
+
+const router = (app) => {
+    app.get('/login', controllers.Account.loginPage);
+    app.post('/login', controllers.Account.login);
+    app.get('/signup', controllers.Account.signupPage);
+    app.post('/signup', controllers.Account.signup);
+  
+    app.get('/logout', controllers.Account.loginPage);
+    app.get('/maker', controllers.Domo.makerPage);
+    app.get('/', controllers.Account.loginPage);
+};
+
+module.exports = router;
